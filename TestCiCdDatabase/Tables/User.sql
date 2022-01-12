6123ed1ee9b5a5e68955a5f54f9c3322bf9017d3
Mitch Saulsberry <9msaul@gmail.com>
2022-01-12 10:51:53 -0600

    initial commit

---
+++ TestCiCdDatabase/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] NVARCHAR(100) NOT NULL, 
+    [First] NVARCHAR(256) NOT NULL, 
+    [Middle] NVARCHAR(256) NULL, 
+    [Last] NCHAR(256) NOT NULL
+)
